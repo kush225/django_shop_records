a143f416b5c672978004e72c3a2b29b11b53eee3
kushagra <kushagra225@gmail.com>
2021-07-25 00:33:09 +0530

    updated code

--- SaleRecord/settings.py
+++ SaleRecord/settings.py
@@ -120,6 +120,20 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
